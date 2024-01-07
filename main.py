986a084fda37908435b9851b161f7328e6b8d8be
Julius Charles <cjmatheka@gmail.com>
2024-01-07 06:30:54 +0300

    Update code for creating data dictionaries

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # import necessary libraries
 from bs4 import BeautifulSoup
 import requests
+import pandas
 
 
 # Getting text from url
@@ -15,8 +16,27 @@ else:
 
 # Parsing the html content to beautiful soup
 soup = BeautifulSoup(html_text, 'lxml')
-movies = soup.find('table', class_='a-bordered a-horizontal-stripes a-size-base a-span12 mojo-body-table mojo-table-annotated scrolling-data-table')
+
+
+# Selecting specific tr class
+def has_target_class(tr):
+    return tr.name == 'tr' and tr.find('td', class_='a-text-right mojo-header-column mojo-truncate '
+                                                    'mojo-field-type-rank')
+
+
+target_tags = soup.find_all(has_target_class)
+for target_tag in target_tags:
+    movie_rank = target_tag.find('td', class_='a-text-right mojo-header-column mojo-truncate mojo-field-type-rank')
+    movie_name = target_tag.find('td', class_='a-text-left mojo-field-type-title')
+    movie_link = target_tag.find('td', class_='a-link-normal')
+    lifetime_gross = target_tag.find('td', class_='a-text-right mojo-field-type-money')
+    release_year = target_tag.find('td', class_='a-text-left mojo-field-type-year')
+
+    print(movie_rank.text)
+    print(movie_name.text)
+    print(lifetime_gross.text)
+    print(release_year.text)
+    print('')
 
 
 
-print(movies)
